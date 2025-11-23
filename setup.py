2a897d63b033ddab901a4474c0d32d5336b9a266
= <=>
2025-11-23 10:04:39 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import  find_packages,setup
+from typing import List
 
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path:str)->List[str]:   
+    """Read the requirements from a file and return as a list."""
+    requirements=[]
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+    
 setup(
     name="mlprojects",               
     version="0.0.1",                  
@@ -7,12 +19,7 @@ setup(
     author_email="shauryasharma4881@gmail.com",
     description="A machine learning projects package",
     packages=find_packages(),         
-    install_requires=[
-        "numpy",
-        "pandas",
-        "scikit-learn",
-        "matplotlib",
-        "seaborn",
-    ],
+    install_requires=get_requirements('requirements.txt'),
+
     python_requires=">=3.8",
 )
