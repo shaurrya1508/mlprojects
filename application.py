bebf34b1f91f7832d1e3fafb2bc593bb104b4764
= <=>
2025-12-23 14:23:49 +0530

    deployment

---
+++ application.py
@@ -0,0 +1,66 @@
+"""
+Without app.py:
+Your model lives only in Python files
+No one can use it
+With app.py:
+Anyone can enter data
+Anyone can get predictions
+Your ML becomes a real application
+"""
+import sys
+import pickle
+from src.exception import CustomException
+
+class PredictPipeline:
+    def predict(self, features):
+        import os  # Add here temporarily
+        model_path = os.path.join("artifacts", "model.pkl")
+        # ... rest of your code
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+
+
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(debug=True, port=5001)        
+
+
